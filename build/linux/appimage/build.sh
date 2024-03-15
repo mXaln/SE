78256d5afb1a5a910975819b0a5d1b0d92561241
vipinpaul <vipin.paul@bridgeconn.com>
2024-03-15 15:55:15 +0530

    Updating build for linux

--- build/linux/appimage/build.sh
+++ build/linux/appimage/build.sh
@@ -19,31 +19,33 @@ if [[ "${VSCODE_ARCH}" == "x64" ]]; then
 
   chmod +x ./pkg2appimage.AppImage
 
-  # ./pkg2appimage.AppImage --appimage-extract && mv ./squashfs-root ./pkg2appimage.AppDir
-  ./pkg2appimage.AppImage --appimage-extract && rsync -a ./squashfs-root ./pkg2appimage.AppDir
-
+  ./pkg2appimage.AppImage --appimage-extract && mv ./squashfs-root ./pkg2appimage.AppDir
+  # ./pkg2appimage.AppImage --appimage-extract && rsync -a ./squashfs-root ./pkg2appimage.AppDir
+  echo "bridge 1"
   # add update's url
   sed -i 's/generate_type2_appimage/generate_type2_appimage -u "gh-releases-zsync|Bridgeconn|oak|latest|*.AppImage.zsync"/' pkg2appimage.AppDir/AppRun
-
+echo "bridge 2"
   # remove check so build in docker can succeed
   sed -i 's/grep docker/# grep docker/' pkg2appimage.AppDir/usr/share/pkg2appimage/functions.sh
-
+echo "bridge 3"
   if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
     sed -i 's|@@NAME@@|ScribeCodex-Insiders|g' recipe.yml
     sed -i 's|@@APPNAME@@|scribecodex-insiders|g' recipe.yml
     sed -i 's|@@ICON@@|vscodium-insiders|g' recipe.yml
   else
     sed -i 's|@@NAME@@|ScribeCodex|g' recipe.yml
+    echo "bridge 4"
     sed -i 's|@@APPNAME@@|scribecodex|g' recipe.yml
+    echo "bridge 5"
     sed -i 's|@@ICON@@|vscodium|g' recipe.yml
   fi
-
+echo "bridge 6"
   bash -ex pkg2appimage.AppDir/AppRun recipe.yml
-
+echo "bridge 7"
   rm -f pkg2appimage-*.AppImage
   rm -rf pkg2appimage.AppDir
   rm -rf VSCodium*
   rm -rf ScribeCodex*
 fi
-
+echo "bridge 8"
 cd "${CALLER_DIR}"

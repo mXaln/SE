044a38339b2800a5dff765d063d3a76f97ff9651
Maxim <murmax82@gmail.com>
2024-05-07 17:19:53 +0400

    embed extensions

--- scribe/scribe.sh
+++ scribe/scribe.sh
@@ -1,26 +1,20 @@
-# pwd is vscode
-echo " start downloading theme"
-# wget https://open-vsx.org/api/sjsepan/sjsepan-redalertish/0.0.2/file/sjsepan.sjsepan-redalertish-0.0.2.vsix
-echo "downloaded theme"
-# if [[ -f  "./sjsepan.sjsepan-redalertish-0.0.2.vsix" ]]; then
-#   unzip ./sjsepan.sjsepan-redalertish-0.0.2.vsix -d ./extensions
-# fi
-# mv "" ""
+jsonfile='{
+  "project-accelerate.codex-editor-extension":"https://open-vsx.org/api/project-accelerate/codex-editor-extension/0.0.12/file/project-accelerate.codex-editor-extension-0.0.12.vsix",
+  "project-accelerate.scripture-language-support": "https://open-vsx.org/api/project-accelerate/scripture-language-support/0.0.6/file/project-accelerate.scripture-language-support-0.0.6.vsix",
+  "project-accelerate.shared-state-store": "https://open-vsx.org/api/project-accelerate/shared-state-store/0.0.2/file/project-accelerate.shared-state-store-0.0.2.vsix",
+  "project-accelerate.pythoninstaller": "https://open-vsx.org/api/project-accelerate/pythoninstaller/0.0.6/file/project-accelerate.pythoninstaller-0.0.6.vsix",
+  "project-accelerate.codex-chat-and-comments": "https://open-vsx.org/api/project-accelerate/codex-chat-and-comments/0.0.2/file/project-accelerate.codex-chat-and-comments-0.0.2.vsix",
+  "project-accelerate.codex-project-manager": "https://open-vsx.org/api/project-accelerate/codex-project-manager/0.0.2/file/project-accelerate.codex-project-manager-0.0.2.vsix",
+  "project-accelerate.codex-scripture-viewer": "https://open-vsx.org/api/project-accelerate/codex-scripture-viewer/0.0.2/file/project-accelerate.codex-scripture-viewer-0.0.2.vsix",
+  "ms-python.python": "https://open-vsx.org/api/ms-python/python/2024.4.1/file/ms-python.python-2024.4.1.vsix"
+}'
 
-# wget https://marketplace.visualstudio.com/_apis/public/gallery/publishers/satyam11/vsextensions/christmas-theme/0.0.1/vspackage
-# unzip ../scribe/satyam11.christmas-theme-0.0.1 -d ./extensions
-
-# wget https://raw.githubusercontent.com/bible-technology/scribe-scripture-editor/development/package.json
-
-jsonfile=$(wget -O - https://raw.githubusercontent.com/vipinpaul/oak-extensions/main/extensions.json)
-# jsonfile='{
-#   "redalertish":"https://open-vsx.org/api/sjsepan/sjsepan-redalertish/0.0.2/file/sjsepan.sjsepan-redalertish-0.0.2.vsix"
-# }'
 echo "$jsonfile" | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' |
 while IFS='=' read -r name url; do
   echo ${url} test ${name}
   wget "$url" -O "$name".zip
   unzip "$name".zip -d ./extensions/"$name"
   mv ./extensions/"$name"/extension/* ./extensions/"$name"/
+  npm install --prefix ./extensions/"$name"
   rm "$name".zip
 done

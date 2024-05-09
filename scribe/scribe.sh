73964dc17480092b9aca042b200b03e508b0f82f
Maxim <murmax82@gmail.com>
2024-05-09 11:48:54 +0400

    extensions source build

--- scribe/scribe.sh
+++ scribe/scribe.sh
@@ -4,17 +4,17 @@ while IFS=': ' read -r name url; do
   echo ${url} test ${name}
   wget -O "$name".zip "$url"
   unzip "$name".zip -d ./extensions/"$name"
-  mv ./extensions/"$name"/extension/* ./extensions/"$name"/
+  # mv ./extensions/"$name"/extension/* ./extensions/"$name"/
   # yarn install --cwd ./extensions/"$name"
-  npm install --prefix ./extensions/"$name"
+  # npm install --prefix ./extensions/"$name"
   rm "$name".zip
 done <<EOF
-project-accelerate.codex-project-manager: https://open-vsx.org/api/project-accelerate/codex-project-manager/0.0.2/file/project-accelerate.codex-project-manager-0.0.2.vsix
-project-accelerate.codex-chat-and-comments: https://open-vsx.org/api/project-accelerate/codex-chat-and-comments/0.0.2/file/project-accelerate.codex-chat-and-comments-0.0.2.vsix
-project-accelerate.codex-scripture-viewer: https://open-vsx.org/api/project-accelerate/codex-scripture-viewer/0.0.2/file/project-accelerate.codex-scripture-viewer-0.0.2.vsix
-project-accelerate.codex-editor-extension: https://open-vsx.org/api/project-accelerate/codex-editor-extension/0.0.12/file/project-accelerate.codex-editor-extension-0.0.12.vsix
-project-accelerate.scripture-language-support: https://open-vsx.org/api/project-accelerate/scripture-language-support/0.0.6/file/project-accelerate.scripture-language-support-0.0.6.vsix
-project-accelerate.shared-state-store: https://open-vsx.org/api/project-accelerate/shared-state-store/0.0.2/file/project-accelerate.shared-state-store-0.0.2.vsix
-project-accelerate.pythoninstaller: https://open-vsx.org/api/project-accelerate/pythoninstaller/0.0.6/file/project-accelerate.pythoninstaller-0.0.6.vsix
-ms-python.python: https://open-vsx.org/api/ms-python/python/2024.4.1/file/ms-python.python-2024.4.1.vsix
+custom.codex-project-manager: https://github.com/genesis-ai-dev/codex-project-manager/archive/refs/heads/main.zip
+custom.codex-chat-and-comments: https://github.com/genesis-ai-dev/codex-comments-and-chat/archive/refs/heads/main.zip
+custom.codex-scripture-viewer: https://github.com/genesis-ai-dev/codex-scripture-viewer/archive/refs/heads/main.zip
+custom.codex-editor-extension: https://github.com/genesis-ai-dev/codex-editor/archive/refs/heads/main.zip
+custom.scripture-language-support: https://github.com/ryderwishart/scripture-language-support/archive/refs/heads/main.zip
+custom.shared-state-store: https://github.com/genesis-ai-dev/shared-state-store/archive/refs/heads/main.zip
+custom.pythoninstaller: https://github.com/genesis-ai-dev/PythonInstaller/archive/refs/heads/main.zip
+custom.python: https://github.com/microsoft/vscode-python/archive/refs/heads/main.zip
 EOF
